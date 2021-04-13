bf0cb9739b4c670b852ac5d1e40476e4e08ec56f
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-04-13 23:48:46 +0100

    update

--- core/src/com/isoterik/cash4life/double_cash/components/Card.java
+++ core/src/com/isoterik/cash4life/double_cash/components/Card.java
@@ -6,7 +6,10 @@ public class Card extends Component {
     public final int number;
 
     public Card(String regionName) {
-        this.number = Integer.parseInt(regionName.replaceAll("[^0-9]", ""));
+        if (regionName.equalsIgnoreCase("cardBack"))
+            this.number = 0;
+        else
+            this.number = Integer.parseInt(regionName.replaceAll("[^0-9]", ""));
     }
 }
 
