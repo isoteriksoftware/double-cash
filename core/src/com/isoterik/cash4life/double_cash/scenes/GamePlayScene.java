1b4e5a4491c23365696027453321a72acd4b4838
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-04-13 23:48:27 +0100

    Update GamePlayScene.java

--- core/src/com/isoterik/cash4life/double_cash/scenes/GamePlayScene.java
+++ core/src/com/isoterik/cash4life/double_cash/scenes/GamePlayScene.java
@@ -3,6 +3,7 @@ package com.isoterik.cash4life.double_cash.scenes;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Array;
+import com.isoterik.cash4life.double_cash.components.Card;
 import com.isoterik.mgdx.GameObject;
 import com.isoterik.mgdx.MinGdx;
 import com.isoterik.mgdx.Scene;
@@ -18,7 +19,14 @@ public class GamePlayScene extends Scene {
         setBackgroundColor(new Color(.1f, .1f, .2f, 1f));
 
         cards = new Array<>();
-        for (TextureAtlas.AtlasRegion region : )
+        for (TextureAtlas.AtlasRegion region : minGdx.assets.getAtlas("spritesheets/cards.atlas")
+                .getRegions()) {
+            GameObject card = newSpriteObject("Card", region);
+            card.addComponent(new Card(region.name));
+            cards.add(card);
+        }
+
+        addGameObject(cards.get(6));
     }
 }
 
