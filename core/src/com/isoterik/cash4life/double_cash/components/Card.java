2b65e72f42d7b253d8abcbe5992b6cc98092ce25
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-04-18 05:26:26 +0100

    update

--- core/src/com/isoterik/cash4life/double_cash/components/Card.java
+++ core/src/com/isoterik/cash4life/double_cash/components/Card.java
@@ -2,38 +2,67 @@ package com.isoterik.cash4life.double_cash.components;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.isoterik.mgdx.Component;
+import com.isoterik.mgdx.GameObject;
+import com.isoterik.mgdx.Scene;
+import com.isoterik.mgdx.Transform;
 import com.isoterik.mgdx.m2d.components.SpriteRenderer;
 
 public class Card extends Component {
-    public static float HIDDEN_SCALE = 0.5f;
+    public static float HIDDEN_SCALE = 0.7f;
+    public static float OPPONENT_SELECTED_SCALE = 0.5f;
 
     public final int number;
 
     public final TextureRegion cardSprite, cardBackSprite;
 
-    public Card(TextureAtlas.AtlasRegion region, TextureRegion cardBackSprite) {
+    public Card(TextureAtlas.AtlasRegion region, TextureRegion cardBackSprite, Scene scene) {
         this.cardBackSprite = cardBackSprite;
         this.cardSprite = region;
         this.number = Integer.parseInt(region.name.replaceAll("[^0-9]", ""));
+        this.scene = scene;
     }
 
     @Override
     public void attach() {
+        super.attach();
         setRevealed(false);
     }
 
     public void setRevealed(boolean isRevealed) {
         SpriteRenderer spriteRenderer = getComponent(SpriteRenderer.class);
+        Vector2 realSize = scene.getMainCamera().getWorldUnits().toWorldUnit(cardSprite);
 
         if (isRevealed) {
             spriteRenderer.setSprite(cardSprite);
-            gameObject.transform.setScale(1, 1);
         }
         else{
             spriteRenderer.setSprite(cardBackSprite);
-            gameObject.transform.setScale(HIDDEN_SCALE, HIDDEN_SCALE);
+            realSize.scl(HIDDEN_SCALE);
         }
+        gameObject.transform.setSize(realSize.x, realSize.y);
+    }
+
+    public void setOpponentSelected() {
+        Vector2 realSize = scene.getMainCamera().getWorldUnits().toWorldUnit(cardSprite);
+        realSize.scl(OPPONENT_SELECTED_SCALE);
+        gameObject.transform.setSize(realSize.x, realSize.y);
+    }
+
+    public boolean isTouched(float touchX, float touchY) {
+        Transform t = gameObject.transform;
+
+        return touchX >= t.getX() && touchX <= t.getX() + getRealWidth() &&
+                touchY >= t.getY() && touchY <= t.getY() + getRealHeight();
+    }
+
+    public float getRealWidth() {
+        return gameObject.transform.getWidth() * gameObject.transform.getScaleX();
+    }
+
+    public float getRealHeight() {
+        return gameObject.transform.getHeight() * gameObject.transform.getScaleY();
     }
 }
 
--- core/src/com/isoterik/cash4life/double_cash/scenes/GamePlayScene.java
+++ core/src/com/isoterik/cash4life/double_cash/scenes/GamePlayScene.java
@@ -3,50 +3,58 @@ package com.isoterik.cash4life.double_cash.scenes;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.FillViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.isoterik.cash4life.double_cash.Constants;
 import com.isoterik.cash4life.double_cash.components.Card;
 import com.isoterik.mgdx.GameObject;
 import com.isoterik.mgdx.MinGdx;
 import com.isoterik.mgdx.Scene;
 import com.isoterik.mgdx.Transform;
-import com.isoterik.mgdx.m2d.components.SpriteRenderer;
+import com.isoterik.mgdx.input.ITouchListener;
+import com.isoterik.mgdx.input.TouchEventData;
+import com.isoterik.mgdx.input.TouchTrigger;
+import com.isoterik.mgdx.m2d.components.debug.BoxDebugRenderer;
 import com.isoterik.mgdx.utils.WorldUnits;
 
 public class GamePlayScene extends Scene {
     private MinGdx minGdx;
     private WorldUnits worldUnits;
 
-    private Array<GameObject> cards;
+    private Array<GameObject> cards, pickedCards;
     private final TextureRegion cardBackSprite;
+
+    private final GameObject table, opponent;
+    private GameObject userChoice, opponentChoice;
 
     public GamePlayScene() {
         minGdx = MinGdx.instance();
         setupCamera();
+
+        //setRenderCustomDebugLines(true);
 
         setBackgroundColor(new Color(.1f, .1f, .2f, 1f));
 
         GameObject bg = newSpriteObject(minGdx.assets.regionForTexture("images/background.png"));
         addGameObject(bg);
 
-        GameObject table = newSpriteObject(minGdx.assets.regionForTexture("images/table.png"));
+        table = newSpriteObject(minGdx.assets.regionForTexture("images/table.png"));
         Transform tableTransform = table.transform;
         tableTransform.setPosition((worldUnits.getWorldWidth() - tableTransform.getWidth())/2f,
                 worldUnits.toWorldUnit(10));
         addGameObject(table);
+        table.addComponent(new BoxDebugRenderer());
 
-        GameObject opponent = newSpriteObject(minGdx.assets.regionForTexture("images/opponent.png"));
+        opponent = newSpriteObject(minGdx.assets.regionForTexture("images/opponent.png"));
         Transform opponentTransform = opponent.transform;
         opponentTransform.setScale(1, 0.8f);
         opponentTransform.setPosition((tableTransform.getX() + tableTransform.getWidth()/2f - worldUnits.toWorldUnit(180)),
-                tableTransform.getY() + tableTransform.getHeight() - worldUnits.toWorldUnit(95));
+                tableTransform.getY() + tableTransform.getHeight() - worldUnits.toWorldUnit(100));
         addGameObject(opponent);
 
         cards = new Array<>();
+        pickedCards = new Array<>();
 
         cardBackSprite = minGdx.assets.getAtlas("spritesheets/cards.atlas").findRegion("shirt_red");
         for (TextureAtlas.AtlasRegion region : minGdx.assets.getAtlas("spritesheets/cards.atlas")
@@ -55,13 +63,14 @@ public class GamePlayScene extends Scene {
                 continue;
 
             GameObject card = newSpriteObject("Card", cardBackSprite);
-            card.addComponent(new Card(region, cardBackSprite));
+            card.addComponent(new Card(region, cardBackSprite, this));
             cards.add(card);
+            card.addComponent(new BoxDebugRenderer());
         }
 
-        GameObject card = cards.get(6);
-        placeInCenter(card, table);
-        addGameObject(card);
+        newGame();
+
+        inputManager.addListener(TouchTrigger.touchDownTrigger(), new CardClickListener());
     }
 
     private void setupCamera() {
@@ -72,12 +81,118 @@ public class GamePlayScene extends Scene {
                 mainCamera.getCamera()), worldUnits);
     }
 
-    private void placeInCenter(GameObject gameObject, GameObject host) {
+    private void placeInCenterOf(GameObject gameObject, GameObject host) {
         Transform gt = gameObject.transform;
         Transform ht = host.transform;
 
         gt.setPosition((ht.getX() + ht.getWidth()/2f) - gt.getWidth()/2f,
                 (ht.getY() + ht.getHeight()/2f) - gt.getHeight()/2f);
+    }
+
+    private void pickRandomCards() {
+        cards.shuffle();
+        pickedCards.clear();
+
+        for (int i = 0; i < 5; i++)
+            pickedCards.add(cards.get(i));
+    }
+
+    private void newGame() {
+        placeCards();
+    }
+
+    private void placeCards() {
+        pickRandomCards();
+
+        // Calculate the position of the middle card
+        GameObject middle = pickedCards.get(2);
+        float mx = (table.transform.getX() + table.transform.getWidth()/2f) - middle.transform.getWidth()/2f;
+        float my = (table.transform.getY() + table.transform.getHeight()/2f) - middle.transform.getHeight()/2f;
+        middle.transform.setPosition(mx, my);
+        addGameObject(middle);
+
+        float spacing = worldUnits.toWorldUnit(10);
+
+        for (int i = 0; i < 2; i++) {
+            GameObject card = pickedCards.get(i);
+            float t = 2 - i;
+            float x = mx - (getRealWidth(card) * t) - spacing * t;
+            card.transform.setPosition(x, my);
+            addGameObject(card);
+        }
+
+        for (int i = 3; i < 5; i++) {
+            GameObject card = pickedCards.get(i);
+            float t = i - 2;
+            float x = mx + (getRealWidth(card) * t) + spacing * t;
+            card.transform.setPosition(x, my);
+            addGameObject(card);
+        }
+    }
+
+    private void cardSelected() {
+        Card card = userChoice.getComponent(Card.class);
+        card.setRevealed(true);
+        placeInCenterOf(userChoice, table);
+        userChoice.transform.position.y = worldUnits.toWorldUnit(10);
+
+        pickedCards.removeValue(userChoice, true);
+        playForOpponent();
+    }
+
+    private void playForOpponent() {
+        opponentChoice = pickedCards.random();
+        Transform ot = opponent.transform;
+        Card card = opponentChoice.getComponent(Card.class);
+        card.setOpponentSelected();
+
+        opponentChoice.transform.setPosition(ot.getX(),
+                ot.getY() - card.getRealHeight());
+
+        pickedCards.removeValue(opponentChoice, true);
+    }
+
+    private GameObject getMaximumPick() {
+        int max = 0;
+        GameObject currentCard = pickedCards.first();
+        for (GameObject card : pickedCards) {
+            if (card.getComponent(Card.class).number > max)
+                currentCard = card;
+        }
+
+        return currentCard;
+    }
+
+    private GameObject getMinimumPick() {
+        int min = 0;
+        GameObject currentCard = pickedCards.first();
+        for (GameObject card : pickedCards) {
+            if (card.getComponent(Card.class).number < min)
+                currentCard = card;
+        }
+
+        return currentCard;
+    }
+
+    private float getRealWidth(GameObject gameObject) {
+        return gameObject.transform.getWidth() * gameObject.transform.getScaleX();
+    }
+
+    private float getRealHeight(GameObject gameObject) {
+        return gameObject.transform.getHeight() * gameObject.transform.getScaleY();
+    }
+
+    public class CardClickListener implements ITouchListener {
+        @Override
+        public void onTouch(String mappingName, TouchEventData touchEventData) {
+            for (GameObject card : pickedCards) {
+                if (card.getComponent(Card.class).isTouched(touchEventData.touchX, touchEventData.touchY)) {
+                    userChoice = card;
+                    cardSelected();
+                    break;
+                }
+            }
+        }
     }
 }
 
