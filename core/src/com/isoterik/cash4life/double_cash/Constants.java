68279735349c780ecc2b201509d0e50fd80bba40
Imran Abdulmalik <isoteriksoftware@gmail.com>
2021-04-26 08:27:47 +0100

    Update Constants.java

--- core/src/com/isoterik/cash4life/double_cash/Constants.java
+++ core/src/com/isoterik/cash4life/double_cash/Constants.java
@@ -3,7 +3,7 @@ package com.isoterik.cash4life.double_cash;
 public final class Constants {
     public static final int GUI_WIDTH = 1400;
     public static final int GUI_HEIGHT = 720;
-    
+
     public static final int MAX_CARDS = 9;
     public static final float WINNING_CHANCE = 0.3f;
     public static final float STARTING_CHANCE = 0.6f;
